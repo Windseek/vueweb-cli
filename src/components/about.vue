f1d1bf944d34378a03a2a66f3da1c33ddf332f0b
windseek <2356778446@qq.com>
2018-06-05 18:06:43 +0800

    about.uve

--- src/components/about.vue
+++ src/components/about.vue
@@ -4,6 +4,17 @@
   </div>
 </template>
 <script>
+export default {
+    data() {
+      return {}
+    },
+    created() {
+      console.log("created是vue生命周期里的一个钩子函数，在vue对象生成之后会触发");
+    },
+    methods: {
+      
+    }
+  }
 </script>
 <style scoped>
 </style>
