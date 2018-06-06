527370838c3430dca9a73486f2904c33b3a54692
windseek <2356778446@qq.com>
2018-06-06 10:16:32 +0800

    about.vue

--- src/components/about.vue
+++ src/components/about.vue
@@ -9,13 +9,14 @@ export default {
       return {}
     },
     created() {
-      console.log("created是vue生命周期里的一个钩子函数，在vue对象生成之后会触发");
-      this.query();
-      console.log("这个是分支");
+      console.log("这个冲突是我故意造成的,created是vue生命周期里的一个钩子函数，在vue对象生成之后会触发");
+      console.log("这个冲突是我故意造成的");
+      console.log("这个冲突是我故意造成的,这个是分支");
     },
     methods: {
       query() {
-        console.log("我习惯性写这个方法，进入页面会请求后台数据，或者是表单，或者是列表")
+        console.log("这个冲突是我故意造成的");
+        console.log("这个冲突是我故意造成的,我习惯性写这个方法，进入页面会请求后台数据，或者是表单，或者是列表")
       }
     }
   }
